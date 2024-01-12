c118bd32d9cc85140bbf1f189257dd719436f902
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-01-12 15:54:52 -0500

    finished section 2

---
+++ NewsApp/Services/ApiService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsApp.Models;
+using Newtonsoft.Json;
+
+namespace NewsApp.Services
+{
+    public class ApiService
+    {
+        public async Task<Root> GetNews()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=70732fb825be93b59eecbf18254fdca0&topic=breaking-news&lang=en");
+
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
